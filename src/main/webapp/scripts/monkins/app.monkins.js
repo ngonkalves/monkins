631884b6630fbbb63bf9f48031e172b0049a5a1c
Goncalves, Nuno (Coriant - PT/Amadora) <nuno.goncalves@coriant.com>
2018-05-21 14:46:51 +0100

    Fix typo that cause application to not load

--- src/main/webapp/scripts/monkins/app.monkins.js
+++ src/main/webapp/scripts/monkins/app.monkins.js
@@ -1,4 +1,4 @@
-var wsURL = "wss://vm-transnet-009.dci.co-int.net:8080/monkins/controller";
+var wsURL = "ws://vm-transnet-009.dci.co-int.net:8080/monkins/controller";
 
 /* global angular */
 var monkins = angular.module('monkins', ['ui.bootstrap', 'ui-notification', 'dndLists']);
