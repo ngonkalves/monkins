c57c71913e792ee32705ee4145f7bba53e90a794
Goncalves, Nuno (Coriant - PT/Amadora) <nuno.goncalves@coriant.com>
2018-05-21 12:09:20 +0100

    Changed monkins url

--- src/main/webapp/scripts/monkins/app.monkins.js
+++ src/main/webapp/scripts/monkins/app.monkins.js
@@ -1,4 +1,4 @@
-var wsURL = "wss://andreistraut.info:8443/monkins/controller";
+var wsURL = "wss://vm-transnet-009.dci.co-int.net:8080/monkins/controller";
 
 /* global angular */
 var monkins = angular.module('monkins', ['ui.bootstrap', 'ui-notification', 'dndLists']);
